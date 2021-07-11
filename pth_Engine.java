607babc2e569a54c647fc5e666f0251cdada1cac
jbdougl2 <julianbdouglas@gmail.com>
2021-07-11 15:55:25 -0700

    Split player input and rule conifg
    Adjusted engine to reference separate files for player input command and core ruleset. Added the ability to roll Dice and set variables using rules

--- pth_Engine.java
+++ pth_Engine.java
@@ -7,12 +7,14 @@ Contains default file parsing calls
 */
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class pth_Engine{
 	//Variables
 	private int mainDiceCap = 0;
 	public String engineType = "";
 	public ArrayList<String[]> ruleSet = new ArrayList<String[]>();
+	public ArrayList<String[]> commandSet = new ArrayList<String[]>();
 	private String[] tempIndividualRule;
 	private String[] splitPlayerInput;
 	private int splitCount;
@@ -25,13 +27,16 @@ public class pth_Engine{
 	//Functions for retrieving structure to parse content into
 	//Starts relevant agents for running the engine (i.e. Ruleset; Load Config Files; Load Game/Generate Character)
 	public void initialiseEngine(String gameToStart){
-		engineParseFile("Rules","Rules");
+		engineParseFile("Rules","Rules", ruleSet);
+		engineParseFile("Rules","Commands", commandSet);
+
+		interpretEngineRules();
 
 		System.out.println("...Engine Loaded");
 	}
 
 	//Parses the rule files using Global Function
-	public void engineParseFile(String type, String fileName){
+	public void engineParseFile(String type, String fileName, ArrayList<String[]> structuretoadd){
 		tempIndividualRule = Global.fileParser.parseFile(type, fileName);
 
 		for (String rule : tempIndividualRule)
@@ -45,10 +50,24 @@ public class pth_Engine{
 				}
 			}
 			//System.out.println(rule);
-			ruleSet.add(rule.split(" ",splitCount+1));
+			structuretoadd.add(rule.split(" ",splitCount+1));
 		}
 	}
 
+	//Adds the Rules to Engine variables
+	private void interpretEngineRules()
+	{
+		for(String[] rule:ruleSet)
+		{
+			switch(rule[0])
+			{
+				case "dice": mainDiceCap = Integer.parseInt(rule[1]);
+				break;
+			}
+		}
+	}
+
+	//Engine wide input interpretation
 	public void interpretInput(String playerInput){
 		if (playerInput.equalsIgnoreCase("quit") || playerInput.equalsIgnoreCase("quit game"))
 		{
@@ -66,17 +85,30 @@ public class pth_Engine{
 			}
 			splitPlayerInput = playerInput.split(" ", splitCount+1);
 
-			for(String[] rule:ruleSet)
+			for(String[] rule:commandSet)
 			{
 				System.out.println("Checking if " + rule[0].toString() + " equals " + splitPlayerInput[0]);
 				if(rule[0].equalsIgnoreCase(splitPlayerInput[0]))
 				{
-					System.out.println(rule.toString());
+					System.out.println(rule[0].toString());
+					System.out.println("Rolling with Rule Cap: " + rollDice());
+					System.out.println("Rolling with Custom Cap: " + rollDice(100));
 				}
 			}
 		}
 	}
 
-	//TODO: Roll Dice function
-	
+	//Rolls the standard check Dice
+	private int rollDice(){
+		Random rn = new Random();
+
+		return rn.nextInt(mainDiceCap);
+	}
+
+	//Rolls a specific Dice
+	private int rollDice(int diceType){
+		Random rn = new Random();
+
+		return rn.nextInt(diceType);
+	}
 }
